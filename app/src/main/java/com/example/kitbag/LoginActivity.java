9216f4bbbc5e516a8205fc826cabd0d5b53aeb85
sarowalhossain <sarowalhossain1997@gmail.com>
2021-07-31 13:17:21 +0600

    Added Signin and SignUp functionality and some bug fixes...

--- app/src/main/java/com/example/kitbag/LoginActivity.java
+++ app/src/main/java/com/example/kitbag/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.kitbag;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -15,18 +16,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.kitbag.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+
+    // For Authentication
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // Initialize FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
 
         // remove search icon and notification icon from appBar
         binding.customAppBar.appbarImageviewSearch.setVisibility(View.GONE);
@@ -62,8 +73,24 @@ public class LoginActivity extends AppCompatActivity {
             binding.EditTextPassword.setError("Required");
             return;
         }
-        startActivity(new Intent(LoginActivity.this,MainActivity.class));
-        finish();
+        // Get user input data
+        String fakeEmail = binding.EditTextContact.getText().toString()+"@gmail.com";
+        String password = binding.EditTextPassword.getText().toString();
+        // Sign in with email and password
+        mAuth.signInWithEmailAndPassword(fakeEmail, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Login Success!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed!", Toast.LENGTH_SHORT).show();
+                            binding.EditTextPassword.requestFocus();
+                        }
+                    }
+                });
     }
 
 
