9a00b376dc892467b6b24a93aef0d983d9ca0b7c
Soyaeeb <monirul15-1742@diu.edu.bd>
2021-10-14 21:50:15 +0600

    Navigation Drawer Bug Fixes...

--- app/src/main/java/com/example/kitbag/authentication/SignUpActivity.java
+++ app/src/main/java/com/example/kitbag/authentication/SignUpActivity.java
@@ -131,7 +131,7 @@ public class SignUpActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.nav_login:
-                        onBackPressed();
+                        finish();
                         break;
                     case R.id.nav_language:
                         Toast.makeText(SignUpActivity.this, "Language", Toast.LENGTH_SHORT).show();
