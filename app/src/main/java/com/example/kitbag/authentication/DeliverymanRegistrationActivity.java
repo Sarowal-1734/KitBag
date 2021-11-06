a6d564080b84742c33137f7fbd4af5f1ddc55495
sarowalhossain <sarowalhossain1997@gmail.com>
2021-11-07 00:14:17 +0600

    Added Deliveryman Registration Functionality...

---
+++ app/src/main/java/com/example/kitbag/authentication/DeliverymanRegistrationActivity.java
@@ -0,0 +1,271 @@
+package com.example.kitbag.authentication;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Rational;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.example.kitbag.R;
+import com.example.kitbag.databinding.ActivityDeliverymanRegistrationBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.r0adkll.slidr.Slidr;
+import com.r0adkll.slidr.model.SlidrInterface;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+
+public class DeliverymanRegistrationActivity extends AppCompatActivity {
+
+    // Binding our activity
+    private ActivityDeliverymanRegistrationBinding binding;
+
+    // Swipe to back
+    private SlidrInterface slidrInterface;
+
+    // For Authentication
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+
+    // FireStore Connection
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final CollectionReference collectionReference = db.collection("Users");
+
+    private static final int REQUEST_CODE = 10;
+    private boolean mPermissions = false;
+
+    private int submitClicked = 0;
+    private ImageCapture imageCapture;
+    private Uri imageUri, imageUriFrontNID, imageUriBackNID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDeliverymanRegistrationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // For Authentication
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        // Hide appBar icons
+        binding.customAppBar.appbarImageviewSearch.setVisibility(View.GONE);
+        binding.customAppBar.appbarNotificationIcon.notificationIcon.setVisibility(View.GONE);
+
+        // Change appBar title
+        binding.customAppBar.appbarTitle.setText("Take NID Photo");
+
+        // Swipe to back
+        slidrInterface = Slidr.attach(this);
+
+        // Adding back arrow in the appBar
+        binding.customAppBar.appbarLogo.setImageDrawable(getResources().getDrawable(R.drawable.ic_arrow_back));
+        binding.customAppBar.appbarLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        // Set drawer menu based on Login/Logout
+        if (currentUser != null) {
+            // User is signed in
+            // Get image from database and set to the appBar
+            collectionReference.document(currentUser.getUid()).get()
+                    .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if (documentSnapshot.getString("imageUrl") != null) {
+                                // Picasso library for download & show image
+                                Picasso.get().load(documentSnapshot.getString("imageUrl")).placeholder(R.drawable.ic_profile).fit().centerCrop().into(binding.customAppBar.appbarImageviewProfile);
+                            }
+                        }
+                    });
+        }
+
+        // Start Permissions checkup
+        init();
+
+        // On submit clicked
+        binding.textViewSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitClicked++;
+                if (submitClicked == 1) {
+                    binding.textViewNID.setText("Back of NID");
+                    imageUriFrontNID = imageUri;
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(DeliverymanRegistrationActivity.this);
+                    builder1.setTitle("Great! Now please capture the back of your NID.");
+                    builder1.setCancelable(false);
+                    builder1.setPositiveButton(
+                            "Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                    reStartCamera();
+                } else {
+                    imageUriBackNID = imageUri;
+                    Intent intent = new Intent(DeliverymanRegistrationActivity.this, NidInformationActivity.class);
+                    intent.putExtra("FrontNID", imageUriFrontNID.toString());
+                    intent.putExtra("BackNID", imageUriBackNID.toString());
+                    startActivity(intent);
+                }
+            }
+        });
+
+        // On reTake Clicked
+        binding.textViewReTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reStartCamera();
+            }
+        });
+
+        // On Capture Image
+        binding.imageViewCaptureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPermissions) {
+                    takePicture();
+                } else {
+                    init();
+                }
+            }
+        });
+
+    } // Ending onCreate
+
+    private void reStartCamera() {
+        binding.imageViewPreviewImage.setImageURI(null);
+        binding.imageViewPreviewImage.setVisibility(View.GONE);
+        binding.previewView.setVisibility(View.VISIBLE);
+        binding.imageViewCaptureImage.setEnabled(true);
+        binding.imageViewCaptureImage.clearColorFilter();
+        binding.textViewSubmit.setEnabled(false);
+        binding.textViewSubmit.setTextColor(Color.GRAY);
+        binding.textViewReTake.setEnabled(false);
+        binding.textViewReTake.setTextColor(Color.GRAY);
+    }
+
+    private void takePicture() {
+        File photoFile;
+        if (submitClicked == 0) {
+            photoFile = new File(getCacheDir().getAbsolutePath(), "NIDFRONT.jpg");
+        } else {
+            photoFile = new File(getCacheDir().getAbsolutePath(), "NIDBACK.jpg");
+        }
+        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(photoFile).build();
+        imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(DeliverymanRegistrationActivity.this),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        imageUri = Uri.fromFile(photoFile);
+                        binding.previewView.setVisibility(View.GONE);
+                        binding.imageViewPreviewImage.setVisibility(View.VISIBLE);
+                        binding.imageViewPreviewImage.setImageURI(imageUri);
+                        binding.imageViewCaptureImage.setEnabled(false);
+                        binding.imageViewCaptureImage.setColorFilter(Color.GRAY);
+                        binding.textViewSubmit.setEnabled(true);
+                        binding.textViewSubmit.setTextColor(Color.BLACK);
+                        binding.textViewReTake.setEnabled(true);
+                        binding.textViewReTake.setTextColor(Color.BLACK);
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Toast.makeText(DeliverymanRegistrationActivity.this, "Failed to capture image!", Toast.LENGTH_SHORT).show();
+                        exception.printStackTrace();
+                    }
+                });
+    }
+
+    private void init() {
+        if (mPermissions) {
+            // Open the Camera Preview
+            startCamera();
+        } else {
+            verifyPermissions();
+        }
+    }
+
+    private void startCamera() {
+        final ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderListenableFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // Camera provider is now guaranteed to be available
+                    ProcessCameraProvider cameraProvider = cameraProviderListenableFuture.get();
+                    // Set up the view finder use case to display camera preview
+                    Preview preview = new Preview.Builder().build();
+                    // Set up the capture use case to allow users to take photos
+                    imageCapture = new ImageCapture.Builder().setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY).build();
+                    // Choose the camera Front or Back
+                    CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+                    // Set image dimension according to the preview
+                    imageCapture.setCropAspectRatio(new Rational(350, 200));
+                    // Connect the preview use case to the previewView
+                    preview.setSurfaceProvider(binding.previewView.getSurfaceProvider());
+                    // Unbind use cases before rebinding
+                    cameraProvider.unbindAll();
+                    // Bind use cases to camera
+                    cameraProvider.bindToLifecycle((LifecycleOwner) DeliverymanRegistrationActivity.this, cameraSelector, preview, imageCapture);
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    public void verifyPermissions() {
+        String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if ((ContextCompat.checkSelfPermission(this.getApplicationContext(), permissions[0]) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this.getApplicationContext(), permissions[1]) == PackageManager.PERMISSION_GRANTED)) {
+            mPermissions = true;
+            init();
+        } else {
+            ActivityCompat.requestPermissions(DeliverymanRegistrationActivity.this, permissions, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            if (mPermissions) {
+                init();
+            } else {
+                verifyPermissions();
+            }
+        }
+    }
+
+}
